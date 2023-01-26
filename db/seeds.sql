89bca851021d884fba6a171b9432040e834663f2
SaintMartyrn <dartagnanhickey@.com>
2023-01-26 14:36:00 -0800

    added schema

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (dept_name)
+VALUES 
+
+INSERT INTO job (title, salary, department_id)
+VALUES
+
+INSERT INTO employee (first_name, last_name, job_id, manager_id)
+VALUES 
+
+
