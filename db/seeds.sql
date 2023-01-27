8cb79a88ded9c9b0642402aed2039768ec48bb05
SaintMartyrn <dartagnanhickey@.com>
2023-01-26 21:35:35 -0800

    fixed db issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,27 @@
 INSERT INTO department (dept_name)
 VALUES 
+('Budget and financing'),
+('Renewable Energy'),
+('City Planning');
 
 INSERT INTO job (title, salary, department_id)
 VALUES
+('Director', 75000, 1),
+('Assistent Dircetor',45000, 1),
+('Petty officer Chief', 18000, 1),
+('Post Master General', 100000, 2),
+('Lead Science developer', 80000, 2),
+('Chief technicians officer', 55000, 3),
+('Unpaid Intern', 0, 1);
 
 INSERT INTO employee (first_name, last_name, job_id, manager_id)
 VALUES 
 
+('Gendo', 'Ikari',1, NULL),
+('Misato', 'Katsuragi',2,1),
+('Shinji', 'Ikari',3,2),
+('Asuka', 'Langley-Soryu',4,NULL),
+('Rei', 'Ayanami',5,4),
+('Ritsuko', 'Akagi',6,NULL),
+('Ryoji', 'Kaji',7,NULL);
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'password123',
+  password: 'Dartagnan72!?',
   database: 'company_db',
 });
 
@@ -106,6 +106,100 @@ connection.connect(err => {
           function (err, res) {
             if (err) throw err;
             console.log('Department added!');
+            startMenu();
+          }
+        );
+      });
+  };
+
+  const addJob = () => {
+    inquirer.prompt([
+        {
+          name: 'jobTitle',
+          type: 'input',
+          message: 'What is the job title?',
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'What is the salary for this job?',
+        },
+        {
+          name: 'deptId',
+          type: 'input',
+          message: 'What is the department ID number?',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'INSERT INTO job (title, salary, department_id) VALUES (?, ?, ?)',
+          [answer.jobTitle, answer.salary, answer.deptId],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Job added!');
+            startMenu();
+          }
+        );
+      });
+  };
+  
+  const addEmployee = () => {
+    inquirer.prompt([
+        {
+          name: 'nameFirst',
+          type: 'input',
+          message: "What is the employee's first name?",
+        },
+        {
+          name: 'nameLast',
+          type: 'input',
+          message: "What is the employee's last name?",
+        },
+        {
+          name: 'jobId',
+          type: 'input',
+          message: "What is the employee's job id?",
+        },
+        {
+          name: 'managerId',
+          type: 'input',
+          message: 'What is the manager Id?',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'INSERT INTO employee (first_name, last_name, job_id, manager_id) VALUES (?, ?, ?, ?)',
+          [answer.nameFirst, answer.nameLast, answer.jobId, answer.managerId],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Employee added!');
+            startMenu();
+          }
+        );
+      });
+  };
+  
+  const updateEmployee = () => {
+    inquirer
+      .prompt([
+        {
+          name: 'id',
+          type: 'input',
+          message: 'Enter employee id',
+        },
+        {
+          name: 'jobId',
+          type: 'input',
+          message: 'Enter new job id',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'UPDATE employee SET job_id=? WHERE id=?',
+          [answer.jobId, answer.id],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Employee updated!');
             startMenu();
           }
         );
